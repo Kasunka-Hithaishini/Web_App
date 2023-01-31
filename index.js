4b3724141038c2719530e3b20c238fe84b4020a6
Kasunka <kasunkahithaishini@gmail.com>
2023-01-31 22:42:44 +0530

    third commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const MongoClient=require('mongodb').MongoClient
 const mongoose=require("mongoose")
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { ObjectId } = require('mongodb')
 const port = 1937
 
 // Initialise the app and setver.
@@ -64,7 +65,7 @@ app.get('/', (req,res) => {
     res.set({
         "Allow-access-Allow-Origin": '*'
     })
-    return res.redirect('SignUp.html');
+    return res.redirect('Home.html');
 })
 
 //Api to display the Home.html page
@@ -79,26 +80,387 @@ app.get('/api/Song', (req,res) => {
     res.sendFile('SongPage.html', {root:'Html'});
 });
 
-//Retrieving data from database
-app.get('/Details',(req,res)=>{
-    const result=database.collection('Details').find({Song_name:"Cham Cham"}).toArray((err,result) =>{
-        if(err) throw err
-        res.send(result)              
-        console.log(result)
-        req.body.Author=result;       
-       
-    })   
-    })
 
-    app.get('/Song',(req,res)=>{
-        const result=databasex.collection('posts').find({}).toArray((err,result) =>{
+
+    /*app.get('/Song',(req,res)=>{
+        const result=databasetest.collection('posts').find({}).toArray((err,result) =>{
             if(err) throw err
             res.send(result)              
             console.log(result)
             req.body.Author=result;       
            
         })   
-        })
+        })*/
+
+        //REtrieving songs and song details from database
+
+        //Asian_Cham_360.mp4 
+        app.get('/chamVone',(req,res)=>{
+            const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7a5a1ac62aa3f1ef7effb')}).toArray((err,result) =>{
+                if(err) throw err
+                res.send(result)              
+                console.log(result)
+                req.body.Author=result;       
+               
+            })   
+            })
+
+            //Asian_Cham_720.mp4
+            app.get('/chamVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7a6147acfbdbd8c9c47e3')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+             //Asian_Cham_Audio.m4a
+            app.get('/chamA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7a69ff73960153ac5a5c0')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Asian_Cham - Song Details
+            app.get('/chamSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"Cham Cham"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                 //Asian_Chogada_360.mp4
+            app.get('/ChogadaVone',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7afd3e0b3eae77e35dc8a')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Asian_Chogada_720.mp4
+            app.get('/ChogadaVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b00d6a55984835ebed33')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                 //Asian_Chogada_Audio.m4a
+            app.get('/ChogadaA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b04f13eb1a32e82c4d76')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Asian_Chagoda - Song Details
+            app.get('/ChagodaSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"Chogada"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Country_Going_360.mp4
+            app.get('/GoingVone',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b08d4897adf1dd375f88')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+             //Country_Going_720.mp4
+            app.get('/GoingVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b0c4ced3e0ce515cacee')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+            //Country_Going_Audio.m4a
+            app.get('/GoingA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b0dd5b22e9e584fb33c1')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Country_Going_Song Details
+            app.get('/GoingSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"Going, Going, Gone"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+            //Country_Human_360.mp4
+            app.get('/HumanVone',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b116250d35b774fd2f0b')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                 //Country_Human_720.mp4
+            app.get('/HumanVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b13006a63bef3f5addf7')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+
+            //Country_Human_Audio.m4a
+            app.get('/HumanA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b15208b41b3b1edb6d8f')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Country_Human_Song Details
+            app.get('/HumanSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"Human"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                 //Kids_Letitgo_360.mp4
+            app.get('/LetVone',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b16ff0d153ec8e9a7334')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Kids_Letitgo_720.mp4
+            app.get('/LetVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b18160f667fa7c4eae89')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Kids_Letitgo_Audio.m4a
+            app.get('/LetA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b1986ccfca5f7214a334')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                
+                //Kids_Letitgo_Song Details
+            app.get('/LetSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"Let it go"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+
+                //Kids_WhenWillMyLife_360.mp4
+            app.get('/WhenVone',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b1b8bda731ba21705b58')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Kids_WhenWillMyLife_720.mp4
+            app.get('/WhenVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b1da2ae2ad550847b297')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Kids_WhenWillMyLife_Audio.m4a
+            app.get('/WhenA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b1f7f12b9a198242a341')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                 //Kids_WhenWillMyLife_Song Details
+            app.get('/WhenSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"When will my life"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Pop_SeeYouAgain_360.mp4
+            app.get('/SeeVone',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b2170b87681c5f7bc1d6')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Pop_SeeYouAgain_720.mp4
+            app.get('/SeeVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b231eac482c165ae1906')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Pop_SeeYouAgain_Audio.m4a
+            app.get('/SeeA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b2513555092525853693')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Kids_SeeYouAgain_Song Details
+            app.get('/SeeSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"See you again"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                 //Pop_WhatMakesYou_360.mp4
+            app.get('/WhatVone',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b26926811e057cd00f85')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                 //Pop_WhatMakesYou_720.mp4
+            app.get('/WhatVtwo',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b28058cdf69b16b6c62d')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                //Pop_WhatMakesYou_Audio.m4a
+            app.get('/WhatA',(req,res)=>{
+                const result=databasetest.collection('fs.chunks').find({files_id:ObjectId('63d7b298ea8719cb2c4c77ac')}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+                
+                //Kids_WhatMakesYou_Song Details
+            app.get('/WhatSDetails',(req,res)=>{
+                const result=databasetest.collection('Details').find({Song_name:"What makes you beautiful"}).toArray((err,result) =>{
+                    if(err) throw err
+                    res.send(result)              
+                    console.log(result)
+                    req.body.Author=result;       
+                   
+                })   
+                })
+
+
+        //REtrieve all songs
+        app.get('/AllSongs',(req,res)=>{
+            const result=databasetest.collection('fs.chunks').find({}).toArray((err,result) =>{
+                if(err) throw err
+                res.send(result)              
+                console.log(result)
+                   
+               
+            })   
+            })
     
 /*app.get('/api/posts',(req, resp) => {
     database.collection('posts').find({}, {name:true}).toArray((err, result) => {
@@ -142,7 +504,7 @@ io.on("connection", function(socket) {
             MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true}, (error, result) =>{
             if(error) throw error
             database = result.db('SongDetails')
-            databasex = result.db('blog')
+            databasetest = result.db('test')
             console.log('Connection successful')
         })
     })
